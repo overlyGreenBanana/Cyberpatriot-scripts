c70533e085656e7a72f5e74a396a553bab0dd55a
ZachScheiter <zachwscheiter@gmail.com>
2025-06-19 19:55:14 -0700

    Update MCP.sh

--- Linux/MCP.sh
+++ Linux/MCP.sh
@@ -3,3 +3,11 @@
 #enable firewall
 sudo ufw enable
 
+read -p "Do you want to run update.sh? (y/n): " yn
+
+case $yn in
+    [Yy]* ) echo "Proceeding...";;
+    [Nn]* ) echo "Exiting..."; exit;;
+    * ) echo "Invalid response. Please answer y or n.";;
+esac
+
